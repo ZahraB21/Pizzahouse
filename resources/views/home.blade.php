d455ebc00fd0797ab0bc6ce105c34807806d1135
Zahra <zbehfarshad@gmail.com>
2020-04-30 15:46:12 -0700

    Modifying login page

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -15,6 +15,8 @@
                     @endif
 
                     You are logged in!
+                    <br />
+                    <a href="/">Order</a>
                 </div>
             </div>
         </div>
