adcf0b080e4c393ca20f48c0f2074e2e146a5a5a
Zahra <zbehfarshad@gmail.com>
2020-04-30 15:33:12 -0700

    Imported Laravel UI + Reach Auth for authenticatio purposes

--- routes/web.php
+++ routes/web.php
@@ -28,3 +28,6 @@ Route::post('/pizzas', 'PizzaController@store');
 Route::get('/pizzas/{id}', 'PizzaController@show');
 
 Route::delete('/pizzas/{id}', 'PizzaController@destroy');
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
