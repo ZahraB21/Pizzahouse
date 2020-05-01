f373eacf953292ed62e1184c7cff5202a52a526a
Zahra <zbehfarshad@gmail.com>
2020-05-01 16:23:50 -0700

    Pizza Page Completed

--- resources/views/Pizzas/index.blade.php
+++ resources/views/Pizzas/index.blade.php
@@ -31,24 +31,40 @@
     </div>
 </div>
 
-
-<div class="flex-center position-ref full-height" id = "Volcano">
+@foreach ($pizzaList as $pizza)
+<div class="flex-center position-ref full-height" id = "{{$pizza->name}}">
     <div class="container">
-        <div class="row " class="p2">
+        <div class="row">
             <div class="col-12 col-md-6">
                 <div class="section flex-center">
-                    <img src="/images/volcano.jpg" alt="Volcano image">
+                    <img src="/images/{{ $pizza->name }}.jpg" alt="Volcano image" class="img-fluid" >
                 </div>
             </div>
-            <div class="col-12 col-md-6">
+            <div class="col-12 col-md-6 col_2">
                 <div class="section">
-                <h1 class="text-center display-3" class="flex-start">{{$pizzaList[0]->name}}</h1>
+                <h1 class="text-center" class="flex-start">{{$pizza->name}} Pizza</h1>
                     <div class="main-content">
-                        Toppings: 
+                        Toppings: <br />
+                        <p class="pl-5">
+                        @foreach ($pizza->toppings as $topping)
+                        {{ $topping }} <span>|</span>
+                        @endforeach
+                        </p>
+                        Base: </br />
+                        <p class="pl-5">{{$pizza->base}}</p>
+                        Type: <br />
+                        <p class="pl-5">{{$pizza->type}} Pizza</p>
+                        <form action="/orders" method="post">
+                            <div class="container flex-center">
+                                <button class="btn btn-dark btn-lg mt-3">Order Now</button>
+                            </div>
+                        </form>
                     </div> 
                 </div>
             </div>
         </div>
     </div>
 </div> 
+<hr>
+@endforeach
 @endsection
