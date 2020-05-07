77654eefd7f2f82b2a80b3d6ef4cec3bc05cc6ae
Zahra <zbehfarshad@gmail.com>
2020-05-06 22:56:02 -0700

    Created the Orders migration

--- database/migrations/2020_05_01_232942_create_orders_table.php
+++ database/migrations/2020_05_01_232942_create_orders_table.php
@@ -15,6 +15,9 @@ class CreateOrdersTable extends Migration
     {
         Schema::create('orders', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('pizza_id')->references('id')->on('pizzas');
+            $table->unsignedInteger('quantity');
+            $table->foreignId('user_id')->references('id')->on('users');
             $table->timestamps();
         });
     }
