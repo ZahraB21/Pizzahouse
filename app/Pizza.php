c710d632ef11ddcfe92903656f123daa05f42dcc
Zahra <zbehfarshad@gmail.com>
2020-05-07 16:20:19 -0700

    Updated the Pizza Model

--- app/Pizza.php
+++ app/Pizza.php
@@ -6,9 +6,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class Pizza extends Model
 {
-    protected $primaryKey = 'id';
-    public $incrementing = true; 
+    
     protected $casts = [
         'toppings' => 'array'
     ];
+
 }
